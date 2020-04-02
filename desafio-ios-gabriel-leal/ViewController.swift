df32a5900a6b4cd4e85680b2335fb785dbb6e10a
Gabriel Sousa Leal <gabrielsousaleal@hotmail.com>
2020-04-02 19:20:23 -0300

    Initial Commit

---
+++ desafio-ios-gabriel-leal/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  desafio-ios-gabriel-leal
+//
+//  Created by Gabriel Sousa on 02/04/20.
+//  Copyright © 2020 Gabriel Sousa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ desafio-ios-gabriel-lealTests/desafio_ios_gabriel_lealTests.swift
@@ -0,0 +1,34 @@
+//
+//  desafio_ios_gabriel_lealTests.swift
+//  desafio-ios-gabriel-lealTests
+//
+//  Created by Gabriel Sousa on 02/04/20.
+//  Copyright © 2020 Gabriel Sousa. All rights reserved.
+//
+
+import XCTest
+@testable import desafio_ios_gabriel_leal
+
+class desafio_ios_gabriel_lealTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
